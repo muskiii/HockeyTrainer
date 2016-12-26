02dc386ac5a7a11b3084094a732883e46d24b337
muskiii <jmfabiano@PRINCIPAL.local>
2016-12-26 15:48:27 -0300

    button unification
    Buttons optimization
    Strings modification

--- app/src/main/java/com/dev/fabianos/hockeyt/MainActivity.java
+++ app/src/main/java/com/dev/fabianos/hockeyt/MainActivity.java
@@ -1,110 +1,80 @@
 package com.dev.fabianos.hockeyt;
 
 import android.app.Activity;
-import android.content.Context;
-import android.graphics.Color;
-import android.os.Handler;
-import android.os.SystemClock;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.os.SystemClock;
 import android.view.View;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.Button;
 import android.widget.Chronometer;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import java.util.Timer;
-import java.util.TimerTask;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends Activity {
-    Chronometer mChronometer;
-    LinearLayout ll;
 
-    static String TAG = "MainActivity";
+    RelativeLayout rl;
+    Chronometer mChronometer;
+    Button start, stop, restart;
+    private long mLastStopTime;
+    private boolean running = false;
+    private boolean stoped = true;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        rl = (RelativeLayout) findViewById(R.id.rl);
 
-        mChronometer = (Chronometer) findViewById(R.id.chronometer);
-        Log.i(TAG, "starting the app");
+        start = (Button) findViewById(R.id.btnStartResume);
+        restart = (Button) findViewById(R.id.btnRestart);
 
-        final Button btnStart = (Button) findViewById(R.id.btnStart);
-        btnStart.setOnClickListener(mStartListener);
+        mChronometer = new Chronometer (MainActivity.this);
 
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams
+                ((int) LayoutParams.WRAP_CONTENT, (int) LayoutParams.WRAP_CONTENT);
 
-        final Button buttonStop = (Button) findViewById(R.id.btnStop);
-        buttonStop.setOnClickListener(mStopListener);
+        params.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        params.setMargins(0, 980, 0, 0);
+        mChronometer.setLayoutParams(params);
+        mChronometer.setTextSize((float) 50);
 
-        final Button buttonReset = (Button) findViewById(R.id.btnRestart);
-        buttonReset.setOnClickListener(mResetListener);
+        rl.addView(mChronometer);
+        restart.setEnabled(false);
 
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                if ( running == false ) {
+                    mChronometer.setBase(SystemClock.elapsedRealtime());
+                    mChronometer.start();
+                    running = true;
+                    stoped = false;
+                    restart.setEnabled(true);
+                }
+                else if( running == true && stoped == true){
+                    long intervalOnPause = (SystemClock.elapsedRealtime() - mLastStopTime);
+                    mChronometer.setBase( mChronometer.getBase() + intervalOnPause );
+                    mChronometer.start();
+                    stoped = false;
+                }else if (running == true && stoped == false){
+                    mChronometer.stop();
+                    mLastStopTime = SystemClock.elapsedRealtime();
+                    stoped = true;
+                }
+            }
+        });
 
-//        final TextView txtTitulo = (TextView) findViewById(R.id.txtTitulo);
-//
-//        btnStart.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Log.i(TAG, "---------------Clicking----------------");
-////                Timer timer = new Timer();
-////                TimerTask onOffTimer = new CustomTimerTask(MainActivity.this);
-////                timer.scheduleAtFixedRate(onOffTimer, 0, 1000);
-//            }
-//        });
+        restart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                mChronometer.setBase(SystemClock.elapsedRealtime());
+                mLastStopTime=0;
+                mChronometer.start();
+                running = true;
+                stoped = false;
+            }
+        });
     }
-
-    View.OnClickListener mStartListener = new View.OnClickListener() {
-        public void onClick(View v) {
-            mChronometer.start();
-            Log.i(TAG, "START");
-
-        }
-    };
-
-    View.OnClickListener mStopListener = new View.OnClickListener() {
-        public void onClick(View v) {
-            mChronometer.stop();
-            Log.i(TAG, "STOP");
-        }
-    };
-
-    View.OnClickListener mResetListener = new View.OnClickListener() {
-        public void onClick(View v) {
-            mChronometer.setBase(SystemClock.elapsedRealtime());
-            Log.i(TAG, "RESET");
-        }
-    };
-
-
-
-
-//    public class CustomTimerTask extends TimerTask {
-//
-//        private Handler mHandler = new Handler();
-//        public int a = 0;
-//        final TextView txtTitulo = (TextView) findViewById(R.id.txtTitulo);
-//
-//        public CustomTimerTask(MainActivity mainActivity) {
-//        }
-//
-//        @Override
-//        public void run() {
-//            new Thread(new Runnable() {
-//                @Override
-//                public void run() {
-//                    mHandler.post(new Runnable() {
-//                        @Override
-//                        public void run() {
-//                            txtTitulo.setText(String.valueOf(a));
-//                            a++;
-//                        }
-//                    });
-//                }
-//            }).start();
-//        }
-//    }
 }
